dc0626a123b70235111ea358c6e4389ba1a749e3
Yaxin <yaxin.fang@gmail.com>
2022-06-01 16:42:10 -0400

    fix bugs; save loss and accuracy for each batch

--- testing.py
+++ testing.py
@@ -7,17 +7,26 @@ from torchtext.data import to_map_style_dataset
 import numpy as np
 import os
 import argparse
+import pickle
+import errno
 
 
-parser = argparse.ArgumentParser(description='Testing the performance of trained stonet with treatment layer')
+parser = argparse.ArgumentParser(description='Testing causal stonet')
 parser.add_argument('--seed', default=1, type=int, help='set seed')
-parser.add_argument('--regression_flag', default=True, type=int,
-                    help='true for regression and false for classification')
+parser.add_argument('--regression', dest='regression_flag', action='store_true', help='true for regression')
+parser.add_argument('--classification', dest='regression_flag', action='store_false', help='false for classification')
+
 parser.add_argument('--layer', default=2, type=int, help='number of hidden layer (not including the treatment layer')
 parser.add_argument('--unit', default=[20, 15], type=int, nargs='+', help='number of hidden unit in each layer')
 parser.add_argument('--sigma', default=[0.001, 0.0001, 0.00001], type=float, nargs='+',
                     help='variance of each layer for the model')
 parser.add_argument('--depth', default=1, type=int, help='number of layers before the treatment layer')
+parser.add_argument('--treat_node', default=1, type=int, help='the position of the treatment variable')
+
+parser.add_argument('--impute_lr', default=[0.0000001], type=float, nargs='+', help='step size in imputation')
+parser.add_argument('--para_lr', default=[0.0001, 0.0000001], type=float, nargs='+',
+                    help='step size in parameter update')
+
 parser.add_argument('--batch_size', default=50, type=int, help='batch size for training')
 parser.add_argument('--path', type=str, help='folder that saves the model parameters')
 
@@ -29,8 +38,11 @@ num_hidden = args.layer
 hidden_dim = args.unit
 sigma_list = args.sigma
 treat_depth = args.depth
+treat_node = args.treat_node
 batch_size = args.batch_size
 PATH = args.path
+impute_lrs = args.impute_lr
+para_lrs = args.para_lr
 
 # load data
 if regression_flag is True:
@@ -40,60 +52,73 @@ else:
 test_data = DataLoader(test_set, batch_size=batch_size)
 
 # get input dim and output dim
-_, _, x_temp = next(iter(test_data))
+_, _, _, _, _, x_temp = next(iter(test_data))
 input_dim = x_temp[0].size(dim=0)
 
 # settings for loss functions
 if regression_flag:
     loss = nn.MSELoss()
     output_dim = 1
+    test_loss_path = []
 else:
     loss = nn.CrossEntropyLoss()
     output_dim = 2  # for binary classification; change accordingly for multi-level classification
+    test_loss_path = []
+    test_accuracy_path = []
 
-# create model
-device = torch.device("cpu")
-# net1 setup
-input_dim1 = input_dim
-output_dim1 = 2
-num_hidden1 = treat_depth
-hidden_dim1 = hidden_dim[:num_hidden1]
-# net2 setup
-input_dim2 = hidden_dim1[-1]
-output_dim2 = output_dim
-num_hidden2 = num_hidden - treat_depth
-hidden_dim2 = hidden_dim[num_hidden1:num_hidden]
 # define network
 np.random.seed(seed)
 torch.manual_seed(seed)
-net1 = Net(num_hidden1, hidden_dim1, input_dim1, output_dim1)
-net2 = Net(num_hidden2, hidden_dim2, input_dim2, output_dim2)
-net1.to(device)
-net2.to(device)
+net = Net(num_hidden, hidden_dim, input_dim, output_dim, treat_depth, treat_node)
+device = torch.device("cpu")
+net.to(device)
+
+# define path to read data
+if regression_flag is True:
+    base_path = os.path.join('.', 'result', 'acic', 'regression')
+else:
+    base_path = os.path.join('.', 'result', 'acic', 'classification')
+spec = str(impute_lrs[0]) + '_' + str(para_lrs[0]) + '_' + str(hidden_dim)
+PATH = os.path.join(base_path, spec)
+
+if not os.path.isdir(PATH):
+    try:
+        os.makedirs(PATH)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(PATH):
+            pass
+        else:
+            raise
 
 # load model parameter
-net1.load_state_dict(torch.load(os.path.join(PATH, 'model1' + '.pt')))
-net2.load_state_dict(torch.load(os.path.join(PATH, 'model2' + '.pt')))
+net.load_state_dict(torch.load(os.path.join(PATH, 'model' + '.pt')))
 
-# calculate loss (and accuaracy)
+# calculate loss (and accuracy)
 test_loss, correct = 0, 0
-num_batches = len(test_data)
-test_size = test_set.__len__()
 with torch.no_grad():
     for batch, (ate, ey1, ey0, y, treat, x) in enumerate(test_data):
-        pred_temp = net1.module_dict[str(0)](x)
-        for layer_index in range(num_hidden1 - 1):
-            pred_temp = net1.module_dict[str(layer_index + 1)](pred_temp)
-        pred = net2.forward(pred_temp)
+        num_sample = y.size(dim=0)
 
-        test_loss += loss(pred, y).item()
+        pred = net.forward(x)
+        test_loss = loss(pred, y).item()
+        test_loss_path[batch] = test_loss
+        print(f"Avg test loss: {test_loss:>8f} \n")
+
         if regression_flag is False:
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-test_loss /= num_batches
-print(f"Avg val loss: {test_loss:>8f} \n")
-if regression_flag is False:
-    correct /= test_size
-    print(f"accuracy: {correct:>8f} \n")
+            correct = (pred.argmax(1) == y).type(torch.float).sum().item()
+            correct /= num_sample
+            test_accuracy_path[batch] = correct
+            print(f"test accuracy: {correct:>8f} \n")
 
 # estimate EY1, EY0, and ATE (need more knowledge about causal inference)
+
+if regression_flag:
+    filename = PATH + 'training_result.txt'
+    f = open(filename, 'wb')
+    pickle.dump(test_loss_path, f)
+    f.close()
+else:
+    filename = PATH + 'training_result.txt'
+    f = open(filename, 'wb')
+    pickle.dump([test_loss_path,test_accuracy_path], f)
+    f.close()
