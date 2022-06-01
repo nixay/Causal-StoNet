552f43348c06afc8e017130d473d3498b64ccc92
Yaxin <yaxin.fang@gmail.com>
2022-06-01 16:40:45 -0400

    change trainig loop to incorporate changes in network; add early stopping

--- training.py
+++ training.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 import argparse
 import errno
-
+import pickle
 
 parser = argparse.ArgumentParser(description='Running stonet with treatment layer')
 parser.add_argument('--seed', default=1, type=int, help='set seed')
@@ -36,18 +36,20 @@ parser.add_argument('--unit', default=[20, 15], type=int, nargs='+', help='numbe
 parser.add_argument('--sigma', default=[0.001, 0.0001, 0.00001], type=float, nargs='+',
                     help='variance of each layer for the model')
 parser.add_argument('--depth', default=1, type=int, help='number of layers before the treatment layer')
+parser.add_argument('--treat_node', default=1, type=int, help='the position of the treatment variable')
 
 # Training Setting
 parser.add_argument('--nepoch', default=100, type=int, help='total number of training epochs')
 parser.add_argument('--batch_size', default=50, type=int, help='batch size for training')
 
-parser.add_argument('--mh_step', default=1, type=int, help='number of SGMCMC step for imputation')
+parser.add_argument('--mh_step', default=1, type=int, help='number of SGHMC step for imputation')
 parser.add_argument('--impute_lr', default=[0.0000001], type=float, nargs='+', help='step size in imputation')
 parser.add_argument('--ita', default=0.5, type=float, help='friction coefficient for SGHMC')
 
 parser.add_argument('--para_lr', default=[0.0001, 0.0000001], type=float, nargs='+',
                     help='step size in parameter update')
 parser.add_argument('--para_momentum', default=0.9, type=float, help='momentum parameter for parameter update')
+parser.add_argument('--patience', default=5, type=int, help='patience for early stopping')
 
 args = parser.parse_args()
 
@@ -67,7 +69,6 @@ device = torch.device("cpu")
 mh_step = args.mh_step
 
 # settings for loss functions
-treat_loss_sum = nn.CrossEntropyLoss(reduction='sum')
 if regression_flag:
     loss = nn.MSELoss()
     loss_sum = nn.MSELoss(reduction='sum')
@@ -117,23 +118,13 @@ num_hidden = args.layer
 treat_depth = args.depth
 hidden_dim = args.unit
 sigma_list = args.sigma
-# net1 setup
-input_dim1 = input_dim
-output_dim1 = 2
-num_hidden1 = treat_depth
-hidden_dim1 = hidden_dim[:num_hidden1]
-# net2 setup
-input_dim2 = hidden_dim1[-1]
-output_dim2 = output_dim
-num_hidden2 = num_hidden - treat_depth
-hidden_dim2 = hidden_dim[num_hidden1:num_hidden]
+treat_node = args.treat_node
+
 # define network
 np.random.seed(seed)
 torch.manual_seed(seed)
-net1 = Net(num_hidden1, hidden_dim1, input_dim1, output_dim1)
-net2 = Net(num_hidden2, hidden_dim2, input_dim2, output_dim2)
-net1.to(device)
-net2.to(device)
+net = Net(num_hidden, hidden_dim, input_dim, output_dim, treat_depth, treat_node)
+net.to(device)
 
 # match the dimension of training parameters
 if len(impute_lrs) == 1 and num_hidden > 1:
@@ -154,23 +145,6 @@ if len(sigma_list) == 1 and num_hidden > 1:
     for i in range(num_hidden):
         sigma_list.append(temp_sigma_list)
 
-# match the training parameters to the networks
-# net1
-sigma_list1 = sigma_list[:num_hidden1 + 1]
-impute_lrs1 = impute_lrs[:num_hidden1]
-para_lrs1 = para_lrs[:num_hidden1 + 1]
-# net2
-sigma_list2 = sigma_list[num_hidden1:num_hidden + 1]
-impute_lrs2 = impute_lrs[num_hidden1:num_hidden]
-para_lrs2 = para_lrs[num_hidden1:num_hidden + 1]
-
-# define optimizer for parameter update
-optimizer_list = []
-for i in range(num_hidden1):
-    optimizer_list.append(SGD(net1.module_dict[str(i)].parameters(), lr=para_lrs1[i], momentum=para_momentum))
-for i in range(num_hidden2):
-    optimizer_list.append(SGD(net2.module_dict[str(i)].parameters(), lr=para_lrs2[i], momentum=para_momentum))
-
 # path to save the result
 if regression_flag is True:
     base_path = os.path.join('.', 'result', data_source, 'regression')
@@ -188,50 +162,46 @@ if not os.path.isdir(PATH):
         else:
             raise
 
+# define optimizer for parameter update
+optimizer_list = []
+for i in range(num_hidden):
+    optimizer_list.append(SGD(net.module_dict[str(i)].parameters(), lr=para_lrs[i], momentum=para_momentum))
 
+
+# setting for early stopping
+patience = 5
+trigger_times = 0
+
+# training the network
 for epoch in range(epochs):
     print("Epoch" + str(epoch))
     # train loop
+    train_loss = 0
+    num_batches = len(train_data)
     for batch, (y, treat, x) in enumerate(train_data):
         # backward imputation
-        for step in range(mh_step):
-            hidden_list1 = net1.backward_imputation(1, impute_lrs1, ita, treat_loss_sum, sigma_list1, x, treat)
-            hidden_list2 = net2.backward_imputation(1, impute_lrs2, ita, loss_sum, sigma_list2, hidden_list1[-1], y)
+        hidden_list = net.backward_imputation(mh_step, impute_lrs, ita, loss_sum, sigma_list, x, y, treat)
 
         # parameter update
-        for p1 in net1.parameters():
-            if p1.grad is not None:
-                p1.grad.zero_()
+        for p in net.parameters():
+            if p.grad is not None:
+                p.grad.zero_()
 
-        for p1 in net2.parameters():
-            if p1.grad is not None:
-                p1.grad.zero_()
-
-        adj_factor = batch_size/input_dim
-        for layer_index in range(num_hidden1):
-            hidden_likelihood1 = -adj_factor * net1.likelihood("p", hidden_list1, layer_index, treat_loss_sum,
-                                                               sigma_list1, x, treat)
+        adj_factor = batch_size / input_dim
+        for layer_index in range(num_hidden):
+            hidden_likelihood = -adj_factor * net.likelihood("p", hidden_list, layer_index, loss_sum, sigma_list, x, y,
+                                                             treat)
             optimizer = optimizer_list[layer_index]
             optimizer.zero_grad()
-            hidden_likelihood1.backward()
+            hidden_likelihood.backward()
             optimizer.step()
 
-        for layer_index in range(num_hidden2):
-            hidden_likelihood2 = -adj_factor * net2.likelihood("p", hidden_list2, layer_index, loss_sum, sigma_list2,
-                                                               hidden_list1[-1], y)
-            optimizer = optimizer_list[num_hidden1 + layer_index]
-            optimizer.zero_grad()
-            hidden_likelihood2.backward()
-            optimizer.step()
+        # accumulate train loss for each batch
+        pred = net.forward(x)
+        train_loss += loss(pred, y).item()
 
-    pred_temp = net1.module_dict[str(0)](x)
-    for layer_index in range(num_hidden1 - 1):
-        pred_temp = net1.module_dict[str(layer_index + 1)](pred_temp)
-    pred = net2.forward(pred_temp)
-
-    train_loss = loss(pred, y).item()
+    train_loss /= num_batches
     train_loss_path[epoch] = train_loss
-
     print(f"Avg train loss: {train_loss:>8f} \n")
 
     # validation loop
@@ -240,11 +210,7 @@ for epoch in range(epochs):
     val_size = val_set.__len__()
     with torch.no_grad():
         for batch, (y, treat, x) in enumerate(val_data):
-            pred_temp = net1.module_dict[str(0)](x)
-            for layer_index in range(num_hidden1 - 1):
-                pred_temp = net1.module_dict[str(layer_index + 1)](pred_temp)
-            pred = net2.forward(pred_temp)
-
+            pred = net.forward(x)
             val_loss += loss(pred, y).item()
             if regression_flag is False:
                 correct += (pred.argmax(1) == y).type(torch.float).sum().item()
@@ -252,10 +218,33 @@ for epoch in range(epochs):
     val_loss /= num_batches
     val_loss_path[epoch] = val_loss
     print(f"Avg val loss: {val_loss:>8f} \n")
+
     if regression_flag is False:
         correct /= val_size
         val_accuracy_path[epoch] = correct
         print(f"accuracy: {correct:>8f} \n")
 
-torch.save(net1.state_dict(), os.path.join(PATH, 'model1' + '.pt'))
-torch.save(net2.state_dict(), os.path.join(PATH, 'model2' + '.pt'))
+    # early stopping
+    if epoch > 0:
+        last_loss = val_loss_path[epoch - 1]
+        current_loss = val_loss
+        if current_loss > last_loss:
+            trigger_times += 1
+            if trigger_times >= patience:
+                print('Early stopping!')
+                break
+        else:
+            trigger_times = 0
+
+torch.save(net.state_dict(), os.path.join(PATH, 'model' + '.pt'))
+
+if regression_flag:
+    filename = PATH + 'training_result.txt'
+    f = open(filename, 'wb')
+    pickle.dump([train_loss_path, val_loss_path], f)
+    f.close()
+else:
+    filename = PATH + 'training_result.txt'
+    f = open(filename, 'wb')
+    pickle.dump([train_loss_path, val_loss_path, val_accuracy_path], f)
+    f.close()
