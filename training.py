fix errors in trian loss calculation

training.py
@@ -9,27 +9,25 @@ import numpy as np
 import os
 import argparse
 import errno
-import pickle
 
 parser = argparse.ArgumentParser(description='Running stonet with treatment layer')
 parser.add_argument('--seed', default=1, type=int, help='set seed')
 
+# regression flag
 parser.add_argument('--regression', dest='regression_flag', action='store_true', help='true for regression')
 parser.add_argument('--classification', dest='regression_flag', action='store_false', help='false for classification')
-
 # data
 parser.add_argument('--data_source', default='sim', type=str,
                     help='specify the name of the data, the other option is acic')
 
 parser.add_argument('--whole_data', dest='subset', action='store_false',
-                    help='for acic data with continuous out come variable, use the whole data to train')
+                    help='for acic data with continuous outcome variable, use the whole data to train')
 parser.add_argument('--subset', dest='subset', action='store_true',
-                    help='for acic data with continuous out come variable, use a subset of data to train')
+                    help='for acic data with continuous outcome variable, use a subset of data to train')
 parser.set_defaults(subset=False)
 
 parser.add_argument('--data_name', default='speed', type=str,
                     help='data name of the acic data with binary outcome variable. The other option is epi')
-
 # model
 parser.add_argument('--layer', default=3, type=int, help='number of hidden layer (not including the treatment layer')
 parser.add_argument('--unit', default=[64, 32, 16], type=int, nargs='+', help='number of hidden unit in each layer')
@@ -44,29 +42,38 @@ parser.add_argument('--batch_size', default=64, type=int, help='batch size for t
 
 parser.add_argument('--mh_step', default=1, type=int, help='number of SGHMC step for imputation')
 parser.add_argument('--impute_lr', default=[0.0000001], type=float, nargs='+', help='step size in imputation')
-parser.add_argument('--ita', default=0.2, type=float, help='friction coefficient for SGHMC')
+parser.add_argument('--ita', default=0.5, type=float, help='friction coefficient for SGHMC')
 
-parser.add_argument('--para_lr', default=[1e-4, 1e-5, 1e-6], type=float, nargs='+',
+parser.add_argument('--para_lr', default=[1e-4, 1e-5, 1e-6, 1e-6], type=float, nargs='+',
                     help='step size in parameter update')
 parser.add_argument('--para_momentum', default=0.9, type=float, help='momentum parameter for parameter update')
 parser.add_argument('--patience', default=5, type=int, help='patience for early stopping')
 
 args = parser.parse_args()
 
-# training parameters
+# imputation parameters
 impute_lrs = args.impute_lr
 ita = args.ita
+mh_step = args.mh_step
+# training parameters
 batch_size = args.batch_size
 para_lrs = args.para_lr
 para_momentum = args.para_momentum
 epochs = args.nepoch
 regression_flag = args.regression_flag
+# data parameters
 data_source = args.data_source
 subset = args.subset
 data_name = args.data_name
+# network setup
+num_hidden = args.layer
+treat_depth = args.depth
+hidden_dim = args.unit
+sigma_list = args.sigma
+treat_node = args.treat_node
+
 seed = args.seed
 device = torch.device("cpu")
-mh_step = args.mh_step
 
 # settings for loss functions
 if regression_flag:
@@ -87,11 +94,13 @@ else:
 # load data for training
 if data_source == 'sim':
     if regression_flag is True:
-        train_set = SimulationData_Cont(1, 500)
-        val_set = SimulationData_Cont(2, 500)
+        data = SimulationData_Cont(1, 1000)
+        train_set, val_set = random_split(data, [500, 500],
+                                          generator=torch.Generator().manual_seed(seed))
     else:
-        train_set = SimulationData_Bin(3, 500)
-        val_set = SimulationData_Bin(4, 500)
+        data = SimulationData_Bin(3, 1000)
+        train_set, val_set = random_split(data, [500, 500],
+                                          generator=torch.Generator().manual_seed(seed))
 if data_source == 'acic':
     if regression_flag is True:
         data = to_map_style_dataset(acic_data('cont', subset, data_name))
@@ -109,16 +118,9 @@ if data_source == 'acic':
 train_data = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 val_data = DataLoader(val_set, batch_size=batch_size, shuffle=True)
 
-# get input dim and output dim
+# get input dim
 _, _, x_temp = next(iter(train_data))
 input_dim = x_temp[0].size(dim=0)
-
-# network setup
-num_hidden = args.layer
-treat_depth = args.depth
-hidden_dim = args.unit
-sigma_list = args.sigma
-treat_node = args.treat_node
 
 # define network
 np.random.seed(seed)
@@ -136,13 +138,13 @@ if len(impute_lrs) == 1 and num_hidden > 1:
 if len(para_lrs) == 1 and num_hidden > 1:
     temp_para_lrs = para_lrs[0]
     para_lrs = []
-    for i in range(num_hidden):
+    for i in range(num_hidden + 1):
         para_lrs.append(temp_para_lrs)
 
 if len(sigma_list) == 1 and num_hidden > 1:
     temp_sigma_list = sigma_list[0]
     sigma_list = []
-    for i in range(num_hidden):
+    for i in range(num_hidden + 1):
         sigma_list.append(temp_sigma_list)
 
 # path to save the result
@@ -150,7 +152,7 @@ if regression_flag is True:
     base_path = os.path.join('.', 'result', data_source, 'regression')
 else:
     base_path = os.path.join('.', 'result', data_source, 'classification', data_name)
-spec = str(impute_lrs) + '_' + str(para_lrs) + '_' + str(hidden_dim)
+spec = str(impute_lrs) + '_' + str(para_lrs) + '_' + str(hidden_dim) + '_' + str(epochs)
 PATH = os.path.join(base_path, spec)
 
 if not os.path.isdir(PATH):
@@ -164,20 +166,23 @@ if not os.path.isdir(PATH):
 
 # define optimizer for parameter update
 optimizer_list = []
-for i in range(num_hidden):
+for i in range(num_hidden + 1):
     optimizer_list.append(SGD(net.module_dict[str(i)].parameters(), lr=para_lrs[i], momentum=para_momentum))
-
 
 # setting for early stopping
 patience = 5
 trigger_times = 0
 
 # training the network
+train_num_batches = len(train_data)
+val_num_batches = len(val_data)
+train_size = train_set.__len__()
+val_size = val_set.__len__()
+
 for epoch in range(epochs):
     print("Epoch" + str(epoch))
     # train loop
-    train_loss = 0
-    num_batches = len(train_data)
+    train_loss, train_correct = 0, 0
     for batch, (y, treat, x) in enumerate(train_data):
         # backward imputation
         hidden_list = net.backward_imputation(mh_step, impute_lrs, ita, loss_sum, sigma_list, x, y, treat)
@@ -187,8 +192,8 @@ for epoch in range(epochs):
             if p.grad is not None:
                 p.grad.zero_()
 
-        adj_factor = batch_size / input_dim
-        for layer_index in range(num_hidden):
+        adj_factor = train_size / batch_size
+        for layer_index in range(num_hidden + 1):
             hidden_likelihood = -adj_factor * net.likelihood("p", hidden_list, layer_index, loss_sum, sigma_list, x, y,
                                                              treat)
             optimizer = optimizer_list[layer_index]
@@ -196,33 +201,38 @@ for epoch in range(epochs):
             hidden_likelihood.backward()
             optimizer.step()
 
-        # accumulate train loss for each batch
-        pred = net.forward(x)
-        train_loss += loss(pred, y).item()
+        with torch.no_grad():
+            pred = net.forward(x)
+            train_loss += loss(pred, y).item()
+            if regression_flag is False:
+                train_correct = (pred.argmax(1) == y).type(torch.float).sum().item()
 
-    train_loss /= num_batches
+    train_loss /= train_num_batches
     train_loss_path[epoch] = train_loss
     print(f"Avg train loss: {train_loss:>8f} \n")
 
+    if regression_flag is False:
+        train_correct /= train_size
+        train_accuracy_path[epoch] = train_correct
+        print(f"train accuracy: {train_correct:>8f} \n")
+
     # validation loop
-    val_loss, correct = 0, 0
-    num_batches = len(val_data)
-    val_size = val_set.__len__()
+    val_loss, val_correct = 0, 0
     with torch.no_grad():
         for batch, (y, treat, x) in enumerate(val_data):
             pred = net.forward(x)
             val_loss += loss(pred, y).item()
             if regression_flag is False:
-                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+                val_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-    val_loss /= num_batches
+    val_loss /= val_num_batches
     val_loss_path[epoch] = val_loss
     print(f"Avg val loss: {val_loss:>8f} \n")
 
     if regression_flag is False:
-        correct /= val_size
-        val_accuracy_path[epoch] = correct
-        print(f"accuracy: {correct:>8f} \n")
+        val_correct /= val_size
+        val_accuracy_path[epoch] = val_correct
+        print(f"val accuracy: {val_correct:>8f} \n")
 
     # early stopping
     if epoch > 0:
@@ -236,15 +246,9 @@ for epoch in range(epochs):
         else:
             trigger_times = 0
 
-torch.save(net.state_dict(), os.path.join(PATH, 'model' + '.pt'))
-
-if regression_flag:
-    filename = PATH + '_training_result.txt'
-    f = open(filename, 'wb')
-    pickle.dump([train_loss_path, val_loss_path], f)
-    f.close()
-else:
-    filename = PATH + '_training_result.txt'
-    f = open(filename, 'wb')
-    pickle.dump([train_loss_path, val_loss_path, val_accuracy_path], f)
-    f.close()
+torch.save(net.state_dict(), os.path.join(PATH, 'model.pt'))
+np.savetxt(os.path.join(PATH, 'train_loss.txt'), train_loss_path, fmt="%s")
+np.savetxt(os.path.join(PATH, 'val_loss.txt'), val_loss_path, fmt="%s")
+if regression_flag is False:
+    np.savetxt(os.path.join(PATH, 'train_accuracy.txt'), train_accuracy_path, fmt="%s")
+    np.savetxt(os.path.join(PATH, 'val_accuracy.txt'), val_accuracy_path, fmt="%s")