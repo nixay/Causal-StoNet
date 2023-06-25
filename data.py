d79a556fb1d5bcca0f55e57e3589f20ce76e0d0d
Yaxin <yaxin.fang@gmail.com>
2023-06-25 11:13:39 -0400

    simulation with missing data

--- data.py
+++ data.py
@@ -1,10 +1,23 @@
 from scipy.stats import truncnorm, bernoulli
-from torch.utils.data import Dataset, random_split, ConcatDataset
+from torch.utils.data import Dataset, random_split, ConcatDataset, Subset
 from sklearn.preprocessing import RobustScaler
 import numpy as np
 import pandas as pd
 import torch
 import os
+
+
+# to-do:
+# Real dataset (twins): the GMM structure is unknown
+# In R: infer the GMM structure; how to check MAR or MNAR? what the output would be?
+# In Python:
+#       1. twins_MAR
+#       2. twins_MNAR
+#       3. data_preprocess: missing value detection for real dataset; separate missing data and observed data
+
+def true_ate(y, treat, y_count):
+    ate = torch.mean(torch.flatten((y - y_count)) * (2*treat-1))
+    return ate
 
 
 def data_preprocess(data, partition_seed, cross_fit_no, cross_val=3, x_scale=True, y_scale=True):
@@ -24,7 +37,7 @@ def data_preprocess(data, partition_seed, cross_fit_no, cross_val=3, x_scale=Tru
     """
     data_size = data.__len__()
     size = int(data_size/cross_val)
-    size_list = [size for i in range(cross_val-1)]
+    size_list = [size]*(cross_val-1)
     size_list.append(data_size-(cross_val-1)*size)
     cross_fit_set = random_split(data, size_list, generator=torch.Generator().manual_seed(partition_seed))
     val_set = cross_fit_set.pop(cross_fit_no-1)
@@ -49,20 +62,48 @@ def data_preprocess(data, partition_seed, cross_fit_no, cross_val=3, x_scale=Tru
     return train_set, val_set, x_scalar, y_scalar
 
 
+def miss_simulated_preprocess(data, partition_seed):
+    """
+    preprocess the simulated dataset with missing values.
+    data: Dataset object
+        output of SimData_Missing
+    seed: int
+        random seed to control the partition of the dataset
+    """
+    # fill the nan values by the median of observed values
+    data.x[data.mis_idx1, 0] = data.x[data.obs_idx1, 0].median()
+    data.x[data.mis_idx4, 3] = data.x[data.obs_idx4, 3].median()
+
+    # separate missing data and observed data: only the training set will contain missing values
+    mis_idx = np.union1d(data.mis_idx1, data.mis_idx4)
+    obs_idx = np.intersect1d(data.obs_idx1, data.obs_idx4, assume_unique=True)
+
+    np.random.seed(partition_seed)
+    np.random.shuffle(obs_idx)
+    val_idx, test_idx, train_obs_idx = np.split(obs_idx, [1000, 2000])
+    train_idx = np.concatenate((mis_idx, train_obs_idx))
+    train_set, val_set, test_set = Subset(data, train_idx), Subset(data, val_idx), Subset(data, test_idx)
+    return train_set, val_set, test_set
+
+
 # Simulation Dataset
 class SimData_Causal(Dataset):
     """
-    generate simulation data with causal relationship; inputs are correlated; there is overlap between inputs for
-    outcome variable and inputs for treatment. Note that when using this dataset, 'shuffle' argument has to be set
-    to be true in dataloader.
+    generate simulation data with causal relationship
+    Note that when using this dataset, 'shuffle' argument has to be set to be true in dataloader.
     input_size: int
         the dimension of input variable
     seed: int
         random seed to generate the dataset
     data_size: int
         sample size of the dataset
+    cor: boolean
+        if True: inputs are correlated
+        if False: inputs are independent
+        The default is true
+
     """
-    def __init__(self, input_size, seed, data_size):
+    def __init__(self, input_size, seed, data_size, cor=True):
         self.data_size = data_size
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -74,9 +115,12 @@ class SimData_Causal(Dataset):
         torch.manual_seed(seed)
         while min(one_count, zero_count) < data_size // 2:
             # generate x
-            ee = truncnorm.rvs(-10, 10)
-            x_temp = truncnorm.rvs(-10, 10, size=input_size) + ee
-            x_temp /= np.sqrt(2)
+            if cor:
+                ee = truncnorm.rvs(-10, 10)
+                x_temp = truncnorm.rvs(-10, 10, size=input_size) + ee
+                x_temp /= np.sqrt(2)
+            else:
+                x_temp = truncnorm.rvs(-10, 10, size=input_size)
 
             # nodes in the first hidden layer
             h11 = np.tanh(2*x_temp[0]+1*x_temp[3])
@@ -134,100 +178,8 @@ class SimData_Causal(Dataset):
         y = self.y[idx]
         x = self.x[idx]
         treat = self.treat[idx]
-        return y, treat, x
-
-    def true_ate(self):
-        ate = torch.mean(torch.flatten((self.y - self.y_count)) * (2*self.treat-1))
-        return ate
-
-
-class SimData_Causal_Ind(Dataset):
-    """
-    generate simulation data with causal relationship; inputs are independent; there is no overlap between inputs for
-    outcome variable and inputs for treatment. Note that when using this dataset, 'shuffle' argument has to be set
-    to be true in dataloader.
-    input_size: int
-        the dimension of input variable
-    seed: int
-        random seed to generate the dataset
-    data_size: int
-        sample size of the dataset
-    """
-    def __init__(self, input_size, seed, data_size):
-        self.data_size = data_size
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        one_count, zero_count = 0, 0  # count of the data points with treatment 1 and treatment 0
-        one_treat, one_x, one_y, one_y_count = ([] for _ in range(4))
-        zero_treat, zero_x, zero_y, zero_y_count = ([] for _ in range(4))
-
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        while min(one_count, zero_count) < data_size // 2:
-            # generate x
-            x_temp = truncnorm.rvs(-10, 10, size=input_size)
-
-            # nodes in the first hidden layer
-            h11 = np.tanh(2*x_temp[0]+1*x_temp[3])
-            h12 = np.tanh(-x_temp[0]-2*x_temp[4])
-            h13 = np.tanh(2*x_temp[1]-2*x_temp[2])
-            h14 = np.tanh(-2*x_temp[3]+1*x_temp[4])
-
-            # nodes in the second hidden layer
-            h21 = np.tanh(-2*h11+h13)
-            h22 = h12-h13
-            h23 = np.tanh(h13-2*h14)
-
-            # generate treatment
-            prob = np.exp(h22)/(1 + np.exp(h22))
-            treat_temp = bernoulli.rvs(p=prob)
-
-            # nodes in the third hidden layer
-            h31 = np.tanh(1*h21-2*treat_temp)
-            h32 = np.tanh(-1*treat_temp+2*h23)
-
-            # counterfactual nodes in the third hidden layer
-            h31_count = np.tanh(1*h21-2*(1-treat_temp))
-            h32_count = np.tanh(-1*(1-treat_temp)+2*h23)
-
-            # generate outcome variable
-            y_temp = -4*h31+2*h32 + np.random.normal(0, 1)
-
-            # generate counterfactual outcome variable
-            y_count_temp = -4*h31_count+2*h32_count + np.random.normal(0, 1)
-
-            if treat_temp == 1:
-                one_count += 1
-                one_x.append(x_temp)
-                one_y.append(y_temp)
-                one_treat.append(treat_temp)
-                one_y_count.append(y_count_temp)
-            else:
-                zero_count += 1
-                zero_x.append(x_temp)
-                zero_y.append(y_temp)
-                zero_treat.append(treat_temp)
-                zero_y_count.append(y_count_temp)
-
-        self.x = torch.FloatTensor(np.array(one_x[:(data_size // 2)] + zero_x[:(data_size // 2)])).to(device)
-        self.y = torch.FloatTensor(np.array(one_y[:(data_size // 2)] + zero_y[:(data_size // 2)]).
-                                   reshape(self.data_size, 1)).to(device)
-        self.treat = torch.FloatTensor(np.array(one_treat[:(data_size // 2)] + zero_treat[:(data_size // 2)])).to(device)
-        self.y_count = torch.FloatTensor(np.array(one_y_count[:(data_size // 2)] + zero_y_count[:(data_size // 2)]).
-                                         reshape(self.data_size, 1)).to(device)
-
-    def __len__(self):
-        return int(self.data_size)
-
-    def __getitem__(self, idx):
-        y = self.y[idx]
-        x = self.x[idx]
-        treat = self.treat[idx]
-        return y, treat, x
-
-    def true_ate(self):
-        ate = torch.mean(torch.flatten((self.y - self.y_count)) * (2*self.treat-1))
-        return ate
+        y_count = self.y_count[idx]
+        return y, treat, x, y_count
 
 
 # ACIC Dataset with Continuous Variable and Homogeneous Treatment Effect
@@ -258,8 +210,7 @@ class acic_data_homo(Dataset):
         self.y = np.array(data['Y'], dtype=np.float32).reshape(self.data_size, 1)
         self.treat = torch.FloatTensor(np.array(data['A'], dtype=np.float32)).to(self.device)
         self.cat_var = np.array(data[cat_col], dtype=np.float32)
-        self.num_var = np.array(data.loc[:, ~data.columns.isin(['Y', 'A', *cat_col])],
-                            dtype=np.float32)
+        self.num_var = np.array(data.loc[:, ~data.columns.isin(['Y', 'A', *cat_col])], dtype=np.float32)
 
     def __len__(self):
         return int(self.data_size)
@@ -306,8 +257,7 @@ class acic_data_hete(Dataset):
         self.y = np.array(data['Y'], dtype=np.float32).reshape(self.data_size, 1)
         self.treat = torch.FloatTensor(np.array(data['A'], dtype=np.float32)).to(self.device)
         self.cat_var = np.array(data[cat_col], dtype=np.float32)
-        self.num_var = np.array(data.loc[:, ~data.columns.isin(['ATE', 'EY1', 'EY0', 'Y', 'A', *cat_col])],
-                                            dtype=np.float32)
+        self.num_var = np.array(data.loc[:, ~data.columns.isin(['ATE', 'EY1', 'EY0', 'Y', 'A', *cat_col])], dtype=np.float32)
 
     def __len__(self):
         return int(self.data_size)
@@ -339,8 +289,7 @@ class PensionData(Dataset):
         self.y = np.array(data['net_tfa'], dtype=np.float32).reshape(self.data_size, 1)
         self.treat = torch.FloatTensor(np.array(data['e401'], dtype=np.float32)).to(self.device)
         self.cat_var = np.array(data[cat_col], dtype=np.float32)
-        self.num_var = np.array(data.loc[:, ~data.columns.isin(['net_tfa', 'e401', *cat_col])],
-                                                  dtype=np.float32)
+        self.num_var = np.array(data.loc[:, ~data.columns.isin(['net_tfa', 'e401', *cat_col])], dtype=np.float32)
 
     def __len__(self):
         return int(self.data_size)
@@ -400,3 +349,136 @@ class TCGA(Dataset):
         treat = self.treat[idx]
         x = torch.FloatTensor(self.num_var[idx]).to(self.device)
         return y, treat, x
+
+
+# Simulation dataset for missing at random
+class SimData_Missing(Dataset):
+    """
+    generate simulation dataset with missing values under different missing patterns;
+    covariates are correlated based on the pre-specified AR(2) process
+    seed: int
+        seed to control randomness
+    miss_pattern: str
+        'mar': missing at random
+        'mnar': missing not at random
+    """
+    def __init__(self, seed, miss_pattern):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.miss_pattern = miss_pattern
+
+        # load covariates
+        csv_name = 'covariates_' + str(seed) + '.csv'
+        x = np.loadtxt(os.path.join('./raw_data/sim_missing', csv_name), delimiter=",", skiprows=1)
+        self.data_size = x.__len__()
+
+        # set seed
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+        # nodes in the first hidden layer
+        h11 = np.tanh(2*x[:, 0]+1*x[:, 3])
+        h12 = np.tanh(-x[:, 0]-2*x[:, 4])
+        h13 = np.tanh(2*x[:, 1]-2*x[:, 2])
+        h14 = np.tanh(-2*x[:, 3]+1*x[:, 4])
+
+        # nodes in the second hidden layer
+        h21 = np.tanh(-2*h11+h13)
+        h22 = h12-h13
+        h23 = np.tanh(h13-2*h14)
+
+        # generate treatment
+        prob = np.exp(h22)/(1 + np.exp(h22))
+        treat = bernoulli.rvs(p=prob)
+
+        # nodes in the third hidden layer
+        h31 = np.tanh(1*h21-2*treat)
+        h32 = np.tanh(-1*treat+2*h23)
+
+        # counterfactual nodes in the third hidden layer
+        h31_count = np.tanh(1*h21-2*(1-treat))
+        h32_count = np.tanh(-1*(1-treat)+2*h23)
+
+        # generate outcome variable
+        y = -4*h31+2*h32 + np.random.normal(0, 1)
+
+        # generate counterfactual outcome variable
+        y_count = -4*h31_count+2*h32_count + np.random.normal(0, 1)
+
+        # generate missing values
+        if self.miss_pattern == 'mnar':  # note that only x1 and x4 has missing values
+            # for x1
+            beta1 = np.concatenate((np.array([4, -2]), np.resize([0.5, 0, -0.5, 0], 100)))
+            score1 = beta1[0] + beta1[1]*treat + np.matmul(x, beta1[2:])  # R1|X, A
+            prob1 = np.exp(score1)/(1+np.exp(score1))
+            obs_ind1 = bernoulli.rvs(p=prob1)  # 1 is observed and 0 is missing
+
+            # for x4
+            beta4 = np.concatenate((np.array([4, -2]), np.resize([0, -0.5, 0, 0.5], 100)))
+            score4 = beta4[0] + beta4[1]*treat + np.matmul(x, beta4[2:])  # R4|X, A
+            prob4 = np.exp(score4)/(1+np.exp(score4))
+            obs_ind4 = bernoulli.rvs(p=prob4)  # 1 is observed and 0 is missing
+
+        elif self.miss_pattern == 'mar':  # randomly delete 10% of the data entries in training set
+            # for x1
+            obs_ind1 = np.array([0]*1000 + [1]*11000)
+            np.random.shuffle(obs_ind1)
+
+            # for x4
+            obs_ind4 = np.array([0]*1000 + [1]*11000)
+            np.random.shuffle(obs_ind4)
+
+        self.obs_idx1 = np.array(np.nonzero(obs_ind1)).flatten()
+        self.mis_idx1 = np.array(np.nonzero(1-obs_ind1)).flatten()
+        x[self.mis_idx1, 0] = np.nan
+
+        self.obs_idx4 = np.array(np.nonzero(obs_ind4)).flatten()
+        self.mis_idx4 = np.array(np.nonzero(1-obs_ind4)).flatten()
+        x[self.mis_idx4, 3] = np.nan
+
+        miss_ind = np.stack((1-obs_ind1, 1-obs_ind4), axis=1)
+
+        self.x = torch.FloatTensor(x).to(self.device)
+        self.treat = torch.FloatTensor(treat).to(self.device)
+        self.y = torch.FloatTensor(y).reshape(self.data_size, 1).to(self.device)
+        self.y_count = torch.FloatTensor(y_count).reshape(self.data_size, 1).to(self.device)
+        self.miss_ind = torch.FloatTensor(miss_ind).to(self.device)
+
+        self.cond_mean = None
+        self.cond_var = None
+        self.dist_miss()
+
+    def __len__(self):
+        return int(self.data_size)
+
+    def __getitem__(self, idx):
+        y = self.y[idx]
+        x = self.x[idx]
+        treat = self.treat[idx]
+        y_count = self.y_count[idx]
+        miss_ind = self.miss_ind[idx]
+        return y, treat, x, miss_ind, y_count
+
+    def dist_miss(self):
+        """
+        calculate conditional distribution for missing covariate
+        """
+        # estimate the conditional mean and conditional variance for missing covariates
+        # x1 neighborhood: x2, x3
+        graph_obs = self.x[self.obs_idx1, :3]
+        graph_mean = graph_obs.mean(dim=0)
+        graph_cov = graph_obs.T.cov()
+        temp = torch.linalg.solve(graph_cov[1:3, 1:3], graph_cov[1:3, 0])
+        cond_mean_x1 = graph_mean[0] + torch.matmul(graph_cov[0, 1:3], temp)
+        cond_cov_x1 = graph_cov[0, 0] - torch.matmul(temp, graph_cov[1:3, 0])
+
+        obs_idx4 = np.intersect1d(self.obs_idx1, self.obs_idx4, assume_unique=True)
+        # x4 neighborhood: x1, x2, x3, x4,
+        graph_obs = torch.index_select(self.x[obs_idx4, :5], 1, torch.LongTensor([3, 0, 1, 2, 4]))
+        graph_mean = graph_obs.mean(dim=0)
+        graph_cov = graph_obs.T.cov()
+        temp = torch.linalg.solve(graph_cov[1:5, 1:5], graph_cov[1:5, 0])
+        cond_mean_x4 = graph_mean[0] + torch.matmul(graph_cov[0, 1:5], temp)
+        cond_cov_x4 = graph_cov[0, 0] - torch.matmul(temp, graph_cov[1:5, 0])
+
+        self.cond_mean = [cond_mean_x1, cond_mean_x4]
+        self.cond_var = [cond_cov_x1, cond_cov_x4]
